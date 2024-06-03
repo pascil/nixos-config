014ec805c948ac6d50ea3097bf668e24a3942fa2
Pascal Leinert <codeberg@pascal-leinert.de>
2024-06-03 20:44:44 +0200

    removed ./modules as it is now ./nixos

--- modules/networking.nix
+++
@@ -1,7 +0,0 @@
-{
-  networking.hostName = "Pascal-X240"; # Define your hostname.
-  networking.useDHCP = false;
-  networking.networkmanager.enable = true;
-  networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
-}
