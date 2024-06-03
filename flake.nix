b583024d0e59310f8be182ae566573b95846e4bc
Pascal Leinert <codeberg@pascal-leinert.de>
2024-06-03 20:42:42 +0200

    Error correction in flake.nix

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
 	  home-manager.nixosModules.home-manager
 	  {
 	    home-manager.useGlobalPkgs = true;
-	    home.manager.useUserPackages = true;
+	    home-manager.useUserPackages = true;
 	    home-manager.users.pl = import ./home.nix;
 	  }
 	];
