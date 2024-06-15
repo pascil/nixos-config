d97f1287d3cc3189caba9e355ea81b694b1e7b3e
Pascal Leinert <codeberg@pascal-leinert.de>
2024-06-15 18:56:55 +0200

    NixOS: 62 current  2024-06-15 18:44:08  24.05.20240612.cc54fb4  6.9.4                           *, HM: 2024-06-15 18:56 : id 23 -> /nix/store/2c3yy9glz8yraivdqd2skwkgwww937b0-home-manager-generation

--- home/modules/shells/fish.nix
+++ home/modules/shells/fish.nix
@@ -5,6 +5,8 @@
       clear = "clear && hyfetch";
       ll = "ls -l";
       fs = "flatpak search --columns=name,application";
+      update-system = " nix flake update && ~/.nixconf/rebuild.sh";
+      collect-garbage = "sudo nix-collect-garbage --delete-old && home-manager expire-generations '-1 day'";
     };
   };
 }
