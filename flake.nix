98bc808348aaf16afe7ecb52967c4f5c921f17c8
Pascal Leinert <codeberg@pascal-leinert.de>
2024-06-21 17:35:07 +0200

    commit fix

--- flake.nix
+++ flake.nix
@@ -2,32 +2,16 @@
   description = "NixOS Flake";
 
   inputs = {
-<<<<<<< HEAD
-    nixpkgs = {url = "github:NixOS/nixpkgs/nixos-unstable";};
-
-    home-manager = {
-      url = "github:nix-community/home-manager/";
-      inputs.nixpkgs.follows = "nixpkgs";
-=======
-    # Stable
     nixpkgs-stable = {url = "github:NixOS/nixpkgs/nixos-24.05";};
 
     home-manager-stable = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs-stable.follows = "nixpkgs";
->>>>>>> d006de4 (unstable / stable test)
     };
     nix-flatpak-stable = {url = "github:gmodena/nix-flatpak/?ref=v0.4.1";};
 
-<<<<<<< HEAD
-    nix-flatpak = {url = "github:gmodena/nix-flatpak/?ref=v0.4.1";};
-
-    nixvim = {
-        url = "github:nix-community/nixvim/";
-=======
     nixvim-stable = {
         url = "github:nix-community/nixvim/nixos-24.05";
->>>>>>> d006de4 (unstable / stable test)
         # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
         inputs.nixpkgs-stable.follows = "nixpkgs";
     };
