d52963b63ce3dfe58a41bd6e643786ffd4739692
Pascal Leinert <codeberg@pascal-leinert.de>
2024-06-21 21:32:53 +0200

    why u no find anything?

--- hosts/Pascal-Server/configuration.nix
+++ hosts/Pascal-Server/configuration.nix
@@ -9,19 +9,19 @@
     imports = [
       ./hardware-configuration.nix
       ./users.nix
-      ../../systemModules/docker
-      ../../systemModules/general
-      ../../systemModules/fstrim
-      ../../systemModules/intel
-      ../../systemModules/ruby
-      ../../systemModules/network
-      ../../systemModules/samba
-      ../../systemModules/zram
-      ../../systemModules/packages
-      ../../systemModules/services
+      ../../systemModules/docker/default.nix
+      ../../systemModules/general/default.nix
+      ../../systemModules/fstrim/default.nix
+      ../../systemModules/intel/default.nix
+      ../../systemModules/ruby/default.nix
+      ../../systemModules/network/default.nix
+      ../../systemModules/samba/default.nix
+      ../../systemModules/zram/default.nix
+      ../../systemModules/packages/default.nix
+      ../../systemModules/services/default.nix
 
     ];
-    
+
     networking.hostName= "Pascal-Server";
     system.stateVersion = "24.05";
   }
